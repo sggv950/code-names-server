13e86e6545ad06ec2190cd8f0181d7f996fefb00
sggv950 <38014323+sggv950@users.noreply.github.com>
2020-05-05 01:21:13 +0300

    commit

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f9fe2a2b70f77944765fed8abb0a58f.js"
+  "/precache-manifest.cc50e2b9f58313b84ad4ebab86091259.js"
 );
 
 self.addEventListener('message', (event) => {
