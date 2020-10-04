4904003e562675e28444afc5ee74c106c9a3cf59
sggv950 <38014323+sggv950@users.noreply.github.com>
2020-10-04 10:41:19 +0300

    fix in kw and api

--- services/keywords-service.js
+++ services/keywords-service.js
@@ -4,7 +4,7 @@ const { FALLBACK_KEYWORDS } = require('./fallback-keywords');
 
 const RANDOM_KW_URL =
   'http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15';
-const RANDOM_KW_API_KEY = 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
+const RANDOM_KW_API_KEY = '';
 
 async function getRandomKeywords(numOfKeywords) {
   try {
